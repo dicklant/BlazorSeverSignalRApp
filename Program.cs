0cabe113c6b464ca26a3c5590d0a1314db2eeaa6
dicklant <dicklant@hotmail.com>
2022-10-06 14:31:05 +0200

    Counter persistent

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<BuzzerState>();
+builder.Services.AddSingleton<CounterState>();
 
 
 builder.Services.AddResponseCompression(opts =>
